c3cc24957bf48bf2adae7b872d494d327ea2a5f7
dhia-trabelsi <trabelsidhia7@gmail.com>
2023-02-26 01:24:40 +0100

    cart page completed

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
@@ -7,10 +8,25 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ProductBoxComponent implements OnInit {
 
   @Input() fullWidthMode = false;
+  product: Product | undefined = {
+    id: 1,
+    title: 'snickers',
+    price: 150,
+    category: 'shoes',
+    description: 'Description',
+    image: 'https://via.placeholder.com/150'
+
+  };
+
+  @Output() addToCart = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddToCart(){
+    this.addToCart.emit(this.product);
   }
 
 }
